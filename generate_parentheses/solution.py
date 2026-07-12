aa9579330f1625c363bf949a1b2c4165b6c743ef
shiv42x <shivam2.patel@ryerson.ca>
2026-07-12 19:19:06 -0400

    add optimal solution

--- generate_parentheses/solution.py
+++ generate_parentheses/solution.py
@@ -17,3 +17,21 @@ class Solution:
 
         dfs("", 0)
         return results
+        """
+        optimal solution:
+
+        def dfs(left, right, s):
+            if len(s) == n * 2:
+                res.append(s)
+                return 
+
+            if left < n:
+                dfs(left + 1, right, s + '(')
+
+            if right < left:
+                dfs(left, right + 1, s + ')')
+
+        res = []
+        dfs(0, 0, '')
+        return res
+        """
