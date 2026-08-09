68e2600e3cb27dc779e28d874f8a2ea1b6966d66
shiv42x <shivam2.patel@ryerson.ca>
2026-08-08 20:56:42 -0400

    add two numbers

---
+++ add_two_numbers/solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        dummy = ListNode()
+        cur = dummy
+        carry = 0
+
+        while l1 or l2 or carry:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
+
+            sum_of_nodes = v1 + v2 + carry             
+            carry = sum_of_nodes // 10
+            sum_of_nodes = sum_of_nodes % 10
+            cur.next = ListNode(sum_of_nodes)
+
+            cur = cur.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+
+        return dummy.next
