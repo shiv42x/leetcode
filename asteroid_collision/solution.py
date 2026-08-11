4c38310e7af62b3de1ee4542de7c3c54b8668d0c
shiv42x <shivam2.patel@ryerson.ca>
2026-08-11 19:50:21 -0400

    asteroid collision

---
+++ asteroid_collision/solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack = []
+
+        for asteroid in asteroids:
+            while stack and stack[-1] > 0 and asteroid < 0:
+                if abs(stack[-1]) < abs(asteroid):
+                    stack.pop()
+                elif abs(stack[-1]) > abs(asteroid):
+                    asteroid = 0
+                    break
+                else:
+                    stack.pop()
+                    asteroid = 0
+                    break
+            if asteroid: stack.append(asteroid)                
+    
+        return stack
