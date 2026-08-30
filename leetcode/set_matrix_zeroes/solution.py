cbafbbb5ef468694ad74adf01e461ee565080e68
shiv42x <shivam2.patel@ryerson.ca>
2026-08-29 21:00:58 -0400

    set matrix zeroes

---
+++ leetcode/set_matrix_zeroes/solution.py
@@ -0,0 +1,23 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        rows = set()
+        cols = set()
+
+        n = len(matrix)
+        m = len(matrix[0])
+
+        for i in range(n):
+            for j in range(m):
+                if matrix[i][j] == 0:
+                    rows.add(i)
+                    cols.add(j)
+
+        for row in rows:
+            matrix[row] = [0] * m
+
+        for col in cols:
+            for i in range(n):
+                matrix[i][col] = 0
